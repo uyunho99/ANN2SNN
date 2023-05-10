cc8de0ce6aa0f7aaf1cd1997efedf4be052d4cbd
bluesunb <sunwoong712@gmail.com>
2023-05-10 11:12:53 +0900

    best model structure for ANN

--- ANN2SNN-master/model_cnn.py
+++ ANN2SNN-master/model_cnn.py
@@ -9,23 +9,32 @@ import torch.nn.functional as F
 
 class Network_ANN(nn.Module):
     def __init__(self):
+        """
+        input size : [bs, 3, 32, 32]
+        """
         super(Network_ANN, self).__init__()
-        self.dropout_rate = 0.7
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, bias=False)
+        self.dropout_rate = 0.5
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1, bias=False)   # [bs, 64, 32, 32]
         self.HalfRect1 = nn.ReLU()
         self.dropout1 = nn.Dropout(self.dropout_rate)
-        self.subsample1 = nn.AvgPool2d(2, 2, 0)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, bias=False)
+        # self.subsample1 = nn.AvgPool2d(2, 2, 0)         # [bs, 64, 16, 16]
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)  # [bs, 64, 32, 32]
         self.HalfRect2 = nn.ReLU()
         self.dropout2 = nn.Dropout(self.dropout_rate)
-        self.subsample2 = nn.AvgPool2d(2, 2, 0)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, bias=False)
+        self.subsample2 = nn.AvgPool2d(2, 2, 0)         # [bs, 128, 16, 16]
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, bias=False)  # [bs, 128, 16, 16]
         self.HalfRect3 = nn.ReLU()
         self.dropout3 = nn.Dropout(self.dropout_rate)
+        # self.subsample3 = nn.AvgPool2d(2, 2, 0)         # [bs, 256, 4, 4]
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1, bias=False)  # [bs, 128, 16, 16]
+        self.HalfRect4 = nn.ReLU()
+        self.dropout4 = nn.Dropout(self.dropout_rate)
+        self.subsample4 = nn.AvgPool2d(2, 2, 0)         # [bs, 128, 8, 8]
+
         self.flatten = nn.Flatten()
 
-        self.fc1 = nn.Linear(64 * 2* 2, 10, bias=False)
-        self.HalfRect4 = nn.ReLU()
+        self.fc1 = nn.Linear(128 * 8 * 8, 10, bias=False)
+        self.HalfRectLast = nn.ReLU()
 
     def to(self, device):
         self.device = device
@@ -34,13 +43,16 @@ class Network_ANN(nn.Module):
 
     def forward(self, input):
         x = self.dropout1(self.HalfRect1(self.conv1(input)))
-        x = self.subsample1(x)
+        # x = self.subsample1(x)
         x = self.dropout2(self.HalfRect2(self.conv2(x)))
         x = self.subsample2(x)
         x = self.dropout3(self.HalfRect3(self.conv3(x)))
+        # x = self.subsample3(x)
+        x = self.dropout4(self.HalfRect4(self.conv4(x)))
+        x = self.subsample4(x)
 
         x = self.flatten(x)
-        x = self.HalfRect4(self.fc1(x))
+        x = self.HalfRectLast(self.fc1(x))
 
         return x
 
@@ -48,25 +60,37 @@ class Network_ANN(nn.Module):
         conv1_weight_max = torch.max(F.relu(self.conv1.weight))
         conv2_weight_max = torch.max(F.relu(self.conv2.weight))
         conv3_weight_max = torch.max(F.relu(self.conv3.weight))
+        conv4_weight_max = torch.max(F.relu(self.conv4.weight))
         fc1_weight_max = torch.max(F.relu(self.fc1.weight))
+
         conv1_activation_max = 0.0
         conv2_activation_max = 0.0
         conv3_activation_max = 0.0
+        conv4_activation_max = 0.0
         fc1_activation_max = 0.0
 
         self.eval()
         for batch_idx, (inputs, targets) in enumerate(train_loader):
             x = inputs.float().to(self.device)
+            
             x = self.dropout1(self.HalfRect2(self.conv1(x)))
             conv1_activation_max = max(conv1_activation_max, torch.max(x))
-            x = self.subsample1(x)
+            # x = self.subsample1(x)
+            
             x = self.dropout2(self.HalfRect2(self.conv2(x)))
             conv2_activation_max = max(conv2_activation_max, torch.max(x))
             x = self.subsample2(x)
+            
             x = self.dropout3(self.HalfRect3(self.conv3(x)))
             conv3_activation_max = max(conv3_activation_max, torch.max(x))
-            x = x.view(-1, 256)
-            x = self.HalfRect4(self.fc1(x))
+            # x = self.subsample3(x)
+
+            x = self.dropout4(self.HalfRect4(self.conv4(x)))
+            conv4_activation_max = max(conv4_activation_max, torch.max(x))
+            x = self.subsample4(x)
+
+            x = x.view(-1, 128 * 8 * 8)
+            x = self.HalfRectLast(self.fc1(x))
             fc1_activation_max = max(fc1_activation_max, torch.max(x))
         self.train()
 
@@ -91,6 +115,12 @@ class Network_ANN(nn.Module):
         self.factor_log.append(1 / applied_inv_factor)
         previous_factor = applied_inv_factor
 
+        scale_factor = max(conv4_weight_max, conv4_activation_max)
+        applied_inv_factor = (scale_factor / previous_factor).item()
+        self.conv4.weight.data = self.conv4.weight.data / applied_inv_factor
+        self.factor_log.append(1 / applied_inv_factor)
+        previous_factor = applied_inv_factor
+
         scale_factor = max(fc1_weight_max, fc1_activation_max)
         applied_inv_factor = (scale_factor / previous_factor).item()
         self.fc1.weight.data = self.fc1.weight.data / applied_inv_factor
@@ -101,22 +131,28 @@ class Network_ANN(nn.Module):
 class Network_SNN(nn.Module):
     def __init__(self, time_window=35, threshold=1.0, max_rate=200):
         super(Network_SNN, self).__init__()
-        self.dropout_rate = 0.7
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, bias=False)
+        self.dropout_rate = 0.5
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1, bias=False)   # [bs, 64, 32, 32]
         self.HalfRect1 = nn.ReLU()
         self.dropout1 = nn.Dropout(self.dropout_rate)
-        self.subsample1 = nn.AvgPool2d(2, 2, 0)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, bias=False)
+        # self.subsample1 = nn.AvgPool2d(2, 2, 0)         # [bs, 64, 16, 16]
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, bias=False)  # [bs, 64, 32, 32]
         self.HalfRect2 = nn.ReLU()
         self.dropout2 = nn.Dropout(self.dropout_rate)
-        self.subsample2 = nn.AvgPool2d(2, 2, 0)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, bias=False)
+        self.subsample2 = nn.AvgPool2d(2, 2, 0)         # [bs, 64, 16, 16]
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, bias=False)  # [bs, 128, 16, 16]
         self.HalfRect3 = nn.ReLU()
         self.dropout3 = nn.Dropout(self.dropout_rate)
+        # self.subsample3 = nn.AvgPool2d(2, 2, 0)         # [bs, 256, 4, 4]
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1, bias=False)  # [bs, 128, 16, 16]
+        self.HalfRect4 = nn.ReLU()
+        self.dropout4 = nn.Dropout(self.dropout_rate)
+        self.subsample4 = nn.AvgPool2d(2, 2, 0)         # [bs, 128, 8, 8]
+
         self.flatten = nn.Flatten()
 
-        self.fc1 = nn.Linear(64 * 2* 2, 10, bias=False)
-        self.HalfRect4 = nn.ReLU()
+        self.fc1 = nn.Linear(128 * 8 * 8, 10, bias=False)
+        self.HalfRectLast = nn.ReLU()
 
         self.threshold = threshold
         self.time_window = time_window
@@ -150,15 +186,19 @@ class Network_SNN(nn.Module):
         spk_input = spksum_input = torch.zeros(
             batch_size, 3, 32, 32, device=self.device)
         mem_post_conv1 = spk_post_conv1 = spksum_post_conv1 = torch.zeros(
-            batch_size, 16, 28, 28, device=self.device)
-        mem_post_subsample1 = spk_post_subsample1 = spksum_post_subsample1 = torch.zeros(
-            batch_size, 16, 14, 14, device=self.device)
+            batch_size, 64, 32, 32, device=self.device)
+        # mem_post_subsample1 = spk_post_subsample1 = spksum_post_subsample1 = torch.zeros(
+        #     batch_size, 16, 14, 14, device=self.device)
         mem_post_conv2 = spk_post_conv2 = spksum_post_conv2 = torch.zeros(
-            batch_size, 32, 10, 10, device=self.device)
+            batch_size, 64, 32, 32, device=self.device)
         mem_post_subsample2 = spk_post_subsample2 = spksum_post_subsample2 = torch.zeros(
-            batch_size, 32, 5, 5, device=self.device)
+            batch_size, 64, 16, 16, device=self.device)
         mem_post_conv3 = spk_post_conv3 = spksum_post_conv3 = torch.zeros(
-            batch_size, 64, 2, 2, device=self.device)
+            batch_size, 128, 16, 16, device=self.device)
+        mem_post_conv4 = spk_post_conv4 = spksum_post_conv4 = torch.zeros(
+            batch_size, 128, 16, 16, device=self.device)
+        mem_post_subsample4 = spk_post_subsample4 = spksum_post_subsample4 = torch.zeros(
+            batch_size, 128, 8, 8, device=self.device)
         mem_post_fc1 = spk_post_fc1 = spksum_post_fc1 = torch.zeros(
             batch_size, 10, device=self.device)
 
@@ -171,12 +211,12 @@ class Network_SNN(nn.Module):
                 t, self.conv1, mem_post_conv1, spk_input)
             spksum_post_conv1 = spksum_post_conv1 + spk_post_conv1
 
-            mem_post_subsample1, spk_post_subsample1 = self.mem_update(
-                t, self.subsample1, mem_post_subsample1, spk_post_conv1)
-            spksum_post_subsample1 = spksum_post_subsample1 + spk_post_subsample1
+            # mem_post_subsample1, spk_post_subsample1 = self.mem_update(
+            #     t, self.subsample1, mem_post_subsample1, spk_post_conv1)
+            # spksum_post_subsample1 = spksum_post_subsample1 + spk_post_subsample1
 
             mem_post_conv2, spk_post_conv2 = self.mem_update(
-                t, self.conv2, mem_post_conv2, spk_post_subsample1)
+                t, self.conv2, mem_post_conv2, spk_post_conv1)
             spksum_post_conv2 = spksum_post_conv2 + spk_post_conv2
 
             mem_post_subsample2, spk_post_subsample2 = self.mem_update(
@@ -187,13 +227,17 @@ class Network_SNN(nn.Module):
                 t, self.conv3, mem_post_conv3, spk_post_subsample2)
             spksum_post_conv3 = spksum_post_conv3 + spk_post_conv3
 
-            mem_post_conv3_, spk_post_subsample3 = self.mem_update(
-                t, self.subsample3, mem_post_conv3_, spk_post_conv3)
-            spksum_post_subsample3 = spksum_post_subsample3 + spk_post_subsample3
+            mem_post_conv4, spk_post_conv4 = self.mem_update(
+                t, self.conv4, mem_post_conv4, spk_post_conv3)
+            spksum_post_conv4 = spksum_post_conv4 + spk_post_conv4
 
-            spk_post_subsample3_ = spk_post_subsample3.view(batch_size, 128)
+            mem_post_subsample4, spk_post_subsample4 = self.mem_update(
+                t, self.subsample4, mem_post_subsample4, spk_post_conv4)
+            spksum_post_subsample4 = spksum_post_subsample4 + spk_post_subsample4
+
+            spk_post_conv4_ = spk_post_subsample4.view(batch_size, 128 * 8 * 8)
             mem_post_fc1, spk_post_fc1 = self.mem_update(
-                t, self.fc1, mem_post_fc1, spk_post_subsample3_)
+                t, self.fc1, mem_post_fc1, spk_post_conv4_)
             spksum_post_fc1 = spksum_post_fc1 + spk_post_fc1
         outputs = spksum_post_fc1 / self.time_window
         return outputs
--- ANN2SNN-master/train_cnn.py
+++ ANN2SNN-master/train_cnn.py
@@ -3,6 +3,7 @@ import torchvision.transforms as transforms
 import time
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import model_cnn as models
 from torch.backends.mps import is_available
 
@@ -10,23 +11,27 @@ from torch.backends.mps import is_available
 def mnist_transform():
     return transforms.Compose([transforms.ToTensor()])
 
+def cifar_transform():
+    return transforms.Compose([transforms.ToTensor()])
+
 
 train_batch_size = 100
-train_dataset = torchvision.datasets.MNIST(
-    root="./Dataset/mnist", train=True, download=True, transform=mnist_transform())
+train_dataset = torchvision.datasets.CIFAR10(root="./Dataset/cifar10", train=True, download=True, transform=cifar_transform())
+val_dataset = torchvision.datasets.CIFAR10(root="./Dataset/cifar10", train=False, download=False, transform=cifar_transform())
+
 train_loader = torch.utils.data.DataLoader(
     train_dataset, batch_size=train_batch_size, shuffle=True)
 
-val_dataset = torchvision.datasets.MNIST(
-    root="./Dataset/mnist", train=False, download=False, transform=mnist_transform())
 val_loader = torch.utils.data.DataLoader(
     train_dataset, batch_size=128, shuffle=False)
 
-device = torch.device("cpu" if not is_available() else "mps")
-learning_rate = 1
+# device = torch.device("cpu" if not is_available() else "mps")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+learning_rate = 1e-3
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-num_epochs = 2
+num_epochs = 20
 acc_record = list([])
 loss_train_record = list([])
 loss_test_record = list([])
@@ -35,7 +40,9 @@ ANN = models.Network_ANN()
 SNN = models.Network_SNN(time_window=40, threshold=1.0, max_rate=400)
 ANN.to(device)
 SNN.to(device)
-optimizer = torch.optim.SGD(ANN.parameters(), lr=learning_rate)
+# optimizer = torch.optim.SGD(ANN.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(ANN.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: 0.95**epoch)
 criterion = nn.MSELoss().to(device)
 
 for epoch in range(num_epochs):
@@ -55,9 +62,10 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         if (i+1) % 100 == 0:
-            print('Epoch [%d/%d], Step [%d/%d], Training Loss: %.5f Time elasped:%.2f s'
-                  % (epoch+1, num_epochs, i+1, len(train_dataset)//train_batch_size, running_loss, time.time()-start_time))
+            print('Epoch [%d/%d], Step [%d/%d], LR: %.4f Training Loss: %.5f Time elasped:%.2f s'
+                  % (epoch+1, num_epochs, i+1, len(train_dataset)//train_batch_size, scheduler.get_lr()[0], running_loss/100, time.time()-start_time))
             running_loss = 0
+    scheduler.step()
     correct = 0
     total = 0
     with torch.no_grad():
@@ -78,6 +86,13 @@ for epoch in range(num_epochs):
 
     correct = 0
     total = 0
+
+# plt.title("Loss Curve")
+# plt.plot(loss_train_record, label="Train Loss")
+# plt.plot(loss_test_record, label="Test Loss")
+# plt.legend(loc='upper right')
+# plt.savefig("loss_curve.png")
+
     SNN.load_state_dict(ANN.state_dict())
     for batch_idx, (inputs, targets) in enumerate(val_loader):
         batch_sz = inputs.size(0)
@@ -89,9 +104,20 @@ for epoch in range(num_epochs):
         correct += float(predicted.eq(targets).sum().cpu().item())
     print("SNN Test Accuracy: %.3f" % (100 * correct / total))
 
+with open('ANN_acc_record.txt', 'w') as f:
+    for item in acc_record:
+        f.write("%s\n" % item)
+with open('ANN_loss_train_record.txt', 'w') as f:
+    for item in loss_train_record:
+        f.write("%s\n" % item)
+with open('ANN_loss_test_record.txt', 'w') as f:
+    for item in loss_test_record:
+        f.write("%s\n" % item)
+
 ANN.normalize_nn(train_loader)
 for i, value in enumerate(ANN.factor_log):
     print('Normalization Factor for Layer %d: %3.5f' % (i, value))
+    
 correct = 0
 total = 0
 SNN_normalized = models.Network_SNN(
