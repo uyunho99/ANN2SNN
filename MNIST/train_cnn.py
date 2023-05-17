f0c5b3037dc36f938fda96345422ed7cbd79c2f7
uyunho99 <uyunho99@gmail.com>
2023-05-17 21:51:36 +0900

    change ANN-2-SNN training code

--- MNIST/train_cnn.py
+++ MNIST/train_cnn.py
@@ -6,30 +6,11 @@ import torch.nn as nn
 import model_cnn as models
 from torch.backends.mps import is_available
 
-
-def mnist_transform():
-    return transforms.Compose([transforms.ToTensor()])
-
-
-train_batch_size = 100
-train_dataset = torchvision.datasets.MNIST(
-    root="./Dataset/mnist", train=True, download=True, transform=mnist_transform())
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=train_batch_size, shuffle=True)
-
-val_dataset = torchvision.datasets.MNIST(
-    root="./Dataset/mnist", train=False, download=False, transform=mnist_transform())
-val_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=128, shuffle=False)
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 learning_rate = 1e-3
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-num_epochs = 20
-acc_record = list([])
-loss_train_record = list([])
-loss_test_record = list([])
+num_epochs = 5
 
 ANN = models.Network_ANN()
 SNN = models.Network_SNN(time_window=40, threshold=1.0, max_rate=400)
@@ -40,15 +21,30 @@ scheduler = torch.optim.lr_scheduler.LambdaLR(
     optimizer=optimizer, lr_lambda=lambda epoch: 0.95**epoch)
 criterion = nn.MSELoss().to(device)
 
-for epoch in range(num_epochs):
+def mnist_transform():
+    return transforms.Compose([transforms.ToTensor()])
+
+def get_data_loaders(train_batch_size):
+    train_dataset = torchvision.datasets.MNIST(
+        root="./Dataset/mnist", train=True, download=True, transform=mnist_transform())
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=train_batch_size, shuffle=True)
+
+    val_dataset = torchvision.datasets.MNIST(
+        root="./Dataset/mnist", train=False, download=False, transform=mnist_transform())
+    val_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=128, shuffle=False)
+    
+    return train_loader, val_loader
+
+def train_model(train_loader, ANN, optimizer, criterion, device):
+    ANN.train()
     running_loss = 0
-    start_time = time.time()
     for i, (inputs, targets) in enumerate(train_loader):
         batch_sz = inputs.size(0)
         ANN.zero_grad()
         optimizer.zero_grad()
         inputs = inputs.float().to(device)
-        current_batch_size = inputs.size(0)
         labels_ = torch.zeros(batch_sz, 10).scatter_(
             1, targets.view(-1, 1), 1).to(device)
         outputs = ANN(inputs)
@@ -56,60 +52,53 @@ for epoch in range(num_epochs):
         running_loss += loss.cpu().item()
         loss.backward()
         optimizer.step()
-        if (i+1) % 100 == 0:
-            print('Epoch [%d/%d], Step [%d/%d], Training Loss: %.5f Time elasped:%.2f s'
-                  % (epoch+1, num_epochs, i+1, len(train_dataset)//train_batch_size, running_loss, time.time()-start_time))
-            running_loss = 0
-    scheduler.step()
+    return running_loss
+
+def test_model(loader, model, criterion, device):
+    model.eval()
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(val_loader):
+        for batch_idx, (inputs, targets) in enumerate(loader):
             batch_sz = inputs.size(0)
             inputs = inputs.float().to(device)
             labels_ = torch.zeros(batch_sz, 10).scatter_(
                 1, targets.view(-1, 1), 1).to(device)
-            outputs = ANN(inputs)
+            outputs = model(inputs)
             targets = targets.to(device)
             loss = criterion(outputs, labels_)
             _, predicted = outputs.max(1)
             total += float(targets.size(0))
             correct += float(predicted.eq(targets).sum().cpu().item())
-    print("ANN Test Accuracy: %.3f" % (100 * correct / total))
-    acc = 100. * float(correct) / float(total)
-    acc_record.append(acc)
+    return correct / total
 
-    correct = 0
-    total = 0
-    snn_start_time = time.time()
+train_loader, val_loader = get_data_loaders(train_batch_size=100)
+
+for epoch in range(num_epochs):
+    start_time = time.time()
+    
+    loss = train_model(train_loader, ANN, optimizer, criterion, device)
+    scheduler.step()
+    
+    print('Epoch [%d/%d], Training Loss: %.5f Time elasped:%.2f s' 
+          % (epoch+1, num_epochs, loss, time.time()-start_time))
+    
+    acc_ann = test_model(val_loader, ANN, criterion, device)
+    print("ANN Test Accuracy: %.3f" % (100 * acc_ann))
+    
     SNN.load_state_dict(ANN.state_dict())
-    for batch_idx, (inputs, targets) in enumerate(val_loader):
-        batch_sz = inputs.size(0)
-        targets = targets.to(device)
-        inputs = inputs.float().to(device)
-        outputs = SNN(inputs)
-        _, predicted = outputs.max(1)
-        total += float(targets.size(0))
-        correct += float(predicted.eq(targets).sum().cpu().item())
+    acc_snn = test_model(val_loader, SNN, criterion, device)
     print("SNN Test Accuracy: %.3f Time elasped:%.2f s" %
-          (100 * correct / total, time.time()-snn_start_time))
+          (100 * acc_snn, time.time()-start_time))
 
 ANN.normalize_nn(train_loader)
 for i, value in enumerate(ANN.factor_log):
     print('Normalization Factor for Layer %d: %3.5f' % (i, value))
 
-correct = 0
-total = 0
 SNN_normalized = models.Network_SNN(
     time_window=40, threshold=1.0, max_rate=1000)
 SNN_normalized.to(device)
 SNN_normalized.load_state_dict(ANN.state_dict())
-for batch_idx, (inputs, targets) in enumerate(val_loader):
-    batch_sz = inputs.size(0)
-    targets = targets.to(device)
-    inputs = inputs.float().to(device)
-    outputs = SNN_normalized(inputs)
-    _, predicted = outputs.max(1)
-    total += float(targets.size(0))
-    correct += float(predicted.eq(targets).sum().cpu().item())
-print("Normalized SNN Test Accuracy: %.3f" % (100 * correct / total))
+
+acc_snn_normalized = test_model(val_loader, SNN_normalized, criterion, device)
+print("Normalized SNN Test Accuracy: %.3f" % (100 * acc_snn_normalized))
