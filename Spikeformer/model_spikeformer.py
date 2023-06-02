f637a39cabf09f467e1d8ec1558429e3527d4083
uyunho99 <uyunho99@gmail.com>
2023-06-02 16:57:58 +0900

    renew the spikeformer model

--- Spikeformer/model_spikeformer.py
+++ Spikeformer/model_spikeformer.py
@@ -4,95 +4,149 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from spikingjelly.clock_driven.neuron import MultiStepLIFNode
 
 import math
 
-class LIFNeuron(nn.Module):
-    def __init__(self, threshold, decay):
-        super().__init__()
-        self.threshold = threshold
-        self.decay = decay
-        self.voltage = 0
-
-    def forward(self, x):
-        spike = th.zeros_like(x)
-        self.voltage = self.voltage * self.decay * (1. - spike) + x
-        spike[self.voltage > self.threshold] = 1.
-        self.voltage[self.voltage > self.threshold] = 0
-
-        return spike
-    
-    
-class SpikeSelfAttention(nn.Module):
-    def __init__(self, dim, num_head, dropout=0.1):
-        super().__init__()
-        self.dim = dim
-        self.num_head = num_head
-        self.dropout = dropout
-
-        self.query = nn.Linear(dim, dim)
-        self.key = nn.Linear(dim, dim)
-        self.value = nn.Linear(dim, dim)
-
-        self.neuron = LIFNeuron(threshold=1.0, decay=0.9)
-
-    def forward(self, x):
-        q = self.neuron(self.query(x))
-        k = self.neuron(self.key(x))
-        v = self.neuron(self.value(x))
-
-        attn_output_weights = F.softmax(
-            q @ k.transpose(-2, -1) / math.sqrt(self.dim), dim=-1)
-        attn_output = attn_output_weights @ v
-        attn_output = self.neuron(attn_output)
-
-        return attn_output
-    
 class PatchEmbedding(nn.Module):
-    def __init__(self, in_channels=3, patch_size=16, emb_size=768):
+    def __init__(self, in_channels=3, embed_dim=192, img_size=32, patch_size=4, drop_rate=0.1):
+        super(PatchEmbedding, self).__init__()
+        self.img_size = img_size # 32
+        self.patch_size = patch_size # 4
+        self.num_patches = (img_size // patch_size) ** 2 # 8*8 = 64
+        self.embed_dim = embed_dim # 192
+        # self.dropout = nn.Dropout(drop_rate) # 0.1
+        
+        self.project = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=embed_dim,
+            kernel_size=patch_size,
+            stride=patch_size,
+        )
+        
+        self.class_token = nn.Parameter(th.zeros(1, 1, embed_dim)) # (1, 1, 192)
+        self.num_patches += 1 # 65
+        self.positional_embedding = nn.Parameter(th.zeros(1, self.num_patches, embed_dim)) # (1, 65, 192)
+        
+        # nn.init.normal_(self.class_token, std=0.02) # class_token 초기화
+        # trunc_normal_(self.positional_embedding, std=.02) # positional_embedding 초기화
+
+    def forward(self, x):
+        B, C, H, W = x.shape # (batch_size, in_channels, img_size, img_size): (batch_size, 3, 32, 32)
+        x = self.project(x)  # (batch_size, embed_dim, num_patches**0.5, num_patches**0.5): (batch_size, 192, 8, 8)
+        x = x.flatten(2)  # (batch_size, embed_dim, num_patches): (batch_size, 192, 64)
+        x = x.transpose(1, 2)  # (batch_size, num_patches, embed_dim): (batch_size, 64, 192)
+        
+        class_tokens = self.class_token.expand(B, -1, -1)  # (batch_size, 1, embed_dim): (batch_size, 1, 192)
+        x = th.cat((class_tokens, x), dim=1)  # (batch_size, num_patches + 1, embed_dim): (batch_size, 65, 192)
+        
+        x += self.positional_embedding  # (batch_size, num_patches + 1, embed_dim): (batch_size, 65, 192)
+        # x = self.dropout(x)
+        return x
+
+class SpikeSelfAttention(nn.Module):
+    def __init__(self, dim=192, num_head=8, qkv_bias=False, dropout=0.1):
         super().__init__()
-        self.patch_size = patch_size
-        self.projection = nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size)
+        self.dim = dim # 임베딩 차원: 192
+        self.num_head = num_head # 헤드 개수: 8
+        self.scale = (dim // num_head) ** -0.5 # 192 / 8 = 24 -> 1/24
+        self.dropout = dropout # 드롭아웃 비율: 0.1
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias) # (192, 576)
+        self.qkv_batchnorm = nn.BatchNorm1d(dim * 3)
+        self.qkv_neuron = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='cupy')
+        
+        self.proj = nn.Linear(dim, dim) # (192, 192)
+        
+        # # 쿼리
+        # self.query = nn.Linear(dim, dim)
+        # self.query_batchnorm = nn.BatchNorm1d(dim)
+        # self.query_neuron = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='cupy')
+        
+        # # 키
+        # self.key = nn.Linear(dim, dim)
+        # self.key_batchnorm = nn.BatchNorm1d(dim)
+        # self.key_neuron = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='cupy')
+        
+        # # 값
+        # self.value = nn.Linear(dim, dim)
+        # self.value_batchnorm = nn.BatchNorm1d(dim)
+        # self.value_neuron = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='cupy')
+
+        # # 출력
+        # self.out = nn.Linear(dim, dim)
+        # self.out_batchnorm = nn.BatchNorm1d(dim)
+        # self.out_neuron = MultiStepLIFNode(tau=2.0, detach_reset=True, backend='cupy')
 
     def forward(self, x):
-        x = self.projection(x)
-        return x.flatten(2).transpose(1, 2)
+        B, N, C = x.shape # (batch_size, num_patches + 1, embed_dim): (batch_size, 65, 192)
 
-class PositionalEncoding(nn.Module):
-    def __init__(self, emb_size, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
+        qkv = self.qkv_neuron(self.qkv_batchnorm(self.qkv(x))) # (batch_size, num_patches + 1, embed_dim * 3): (batch_size, 65, 576)
+        qkv = qkv.reshape(B, N, 3, self.num_head, C // self.num_head).permute(2, 0, 3, 1, 4) # (3, batch_size, num_head, num_patches + 1, embed_dim // num_head): (3, batch_size, 8, 65, 24)
+        q, k, v = qkv.unbind(dim=0) # (batch_size, num_head, num_patches + 1, embed_dim // num_head): (batch_size, 8, 65, 24)
+        # q = self.neuron(self.query(x)) 
+        # k = self.neuron(self.key(x))
+        # v = self.neuron(self.value(x))
 
-        # Compute the positional encodings once in log space.
-        pe = th.zeros(max_len, emb_size)
-        position = th.arange(0, max_len, dtype=th.float).unsqueeze(1)
-        div_term = th.exp(th.arange(0, emb_size, 2).float() * -(math.log(10000.0) / emb_size))
-        pe[:, 0::2] = th.sin(position * div_term)
-        pe[:, 1::2] = th.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+        attn = (q @ k.transpose(-2, -1)) * self.scale # (batch_size, num_head, num_patches + 1, num_patches + 1): (batch_size, 8, 65, 65)
+        attn = attn.softmax(dim=-1) # (batch_size, num_head, num_patches + 1, num_patches + 1): (batch_size, 8, 65, 65)
+        attn = self.dropout(attn) # (batch_size, num_head, num_patches + 1, num_patches + 1): (batch_size, 8, 65, 65)
+        
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C) # (batch_size, num_patches + 1, embed_dim): (batch_size, 65, 192)
+        x = self.proj(x) # (batch_size, num_patches + 1, embed_dim): (batch_size, 65, 192)
+        x = self.dropout(x) # (batch_size, num_patches + 1, embed_dim): (batch_size, 65, 192)
+        return x # (batch_size, num_patches + 1, embed_dim): (batch_size, 65, 192)
 
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
+class MultiLayerPerceptron(nn.Module):
+    def __init__(self, in_features, hidden_features, output_features, bias=True, dropout=0.1):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias) # (192, 768)
+        self.fc2 = nn.Linear(hidden_features, output_features, bias=bias) # (768, 192)
+        self.activation = nn.GELU()
+        self.dropout = nn.Dropout(dropout)
     
-class SpikingTransformerEncoderLayer(nn.TransformerEncoderLayer):
-    def __init__(self, dim, num_head, dim_feedforward=2048, dropout=0.1):
-        super().__init__(dim, num_head, dim_feedforward, dropout)
-        self.self_attn = SpikeSelfAttention(dim, num_head, dropout)
+    def forward(self, x):
+        x = self.fc1(x) # (batch_size, num_patches + 1, hidden_features): (batch_size, 65, 768)
+        x = self.activation(x)
+        x = self.dropout(x)
+        x = self.fc2(x) # (batch_size, num_patches + 1, output_features): (batch_size, 65, 192)
+        x = self.activation(x)
+        x = self.dropout(x)
+        return x
+    
+class TransformerEncoder(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, dropout=0.1,
+                 activation=nn.GELU):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.attention = SpikeSelfAttention(dim, num_heads, qkv_bias, dropout) # (192, 8)
+        self.mlp = MultiLayerPerceptron(dim, int(dim * mlp_ratio), dim, dropout=dropout) # (192, 768, 192)
+        
+    def forward(self, x):
+        x += self.attention(self.norm1(x))
+        x += self.mlp(self.norm2(x))
+        return x
         
 class SpikingViT(nn.Module):
-    def __init__(self, in_channels, patch_size, emb_size, img_size, num_layers, num_heads, num_classes):
+    def __init__(self, img_size=32, patch_size=4, in_channels=3, embed_dim=192, depth=12, num_heads=8, 
+                 mlp_ratio=4., qkv_bias=False, dropout=0.1, num_classes=10):
         super().__init__()
-        self.patch_embedding = PatchEmbedding(in_channels, patch_size, emb_size)
-        self.pos_embedding = PositionalEncoding(emb_size, max_len=(img_size//patch_size)**2)
-        self.transformer_encoder = nn.TransformerEncoder(SpikingTransformerEncoderLayer(emb_size, num_heads), num_layers)
-        self.classifier = nn.Linear(emb_size, num_classes)
+        self.num_classes = num_classes # 10
+        self.patch_size = patch_size # 4
+        self.embed_dim = embed_dim # 192
+        self.num_patches = (img_size // patch_size) ** 2 # 8*8 = 64
 
+        self.patch_embedding = PatchEmbedding(in_channels, embed_dim, img_size, patch_size, dropout) # (3, 192, 32, 4, 0.1)
+        self.blocks = nn.Sequential(*[
+            TransformerEncoder(embed_dim, num_heads, mlp_ratio, qkv_bias, dropout) for _ in range(depth) # (192, 8, 4.0, False, 0.1) * 12
+        ])
+        self.norm = nn.LayerNorm(embed_dim)
+        self.head = nn.Linear(embed_dim, num_classes) # (192, 10)
+        
     def forward(self, x):
         x = self.patch_embedding(x)
-        x = self.pos_embedding(x)
-        x = self.transformer_encoder(x)
-        x = x.mean(dim=1)
-        x = self.classifier(x)
+        x = self.blocks(x)
+        x = self.norm(x)
+        x = self.head(x)[:, 0]
         return x
--- Spikeformer/train_spikeformer.py
+++ Spikeformer/train_spikeformer.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -7,60 +9,78 @@ import model_spikeformer as model
 # Load CIFAR-10 dataset
 transform = transforms.Compose(
     [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+     transforms.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))])
 
-trainset = torchvision.datasets.CIFAR10(root='./Data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
-                                          shuffle=True, num_workers=1)
+train_set = torchvision.datasets.CIFAR10(root='./Data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_set, batch_size=32, shuffle=True, num_workers=1)
 
-testset = torchvision.datasets.CIFAR10(root='./Data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=32,
-                                         shuffle=False, num_workers=1)
+test_set = torchvision.datasets.CIFAR10(root='./Data', train=False, download=True, transform=transform)
+test_loader = DataLoader(test_set, batch_size=32, shuffle=False, num_workers=1)
 
 # Initialize the model
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-model = model.SpikingViT(in_channels=3, patch_size=4, emb_size=768, img_size=32, num_layers=6, num_heads=8, num_classes=10).to(device)
+SpikingViT = model.SpikingViT().to(device)
 
 # Loss function and optimizer
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(SpikingViT.parameters(), lr=0.01, weight_decay=0.0001) # weight_decay: L2 규제
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 250], gamma=0.1) # 학습률 감소
 
-# Training
-for epoch in range(10):  # Loop over the dataset multiple times
+def train_one_epoch(model, loader, device, criterion, optimizer):
+    model.train()
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data[0].to(device), data[1].to(device)
-
-        # Zero the parameter gradients
+    correct_predictions = 0.0
+    
+    for inputs, labels in loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        
         optimizer.zero_grad()
-
-        # Forward + backward + optimize
         outputs = model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
-        # Print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # Print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+        running_loss += loss.item() * inputs.size(0)
+        _, preds = torch.max(outputs, 1)
+        correct_predictions += (preds == labels).sum().item()
 
-print('Finished Training')
+    epoch_loss = running_loss / len(loader.dataset)
+    epoch_acc = correct_predictions / len(loader.dataset)
+    
+    return epoch_loss, epoch_acc
 
-# Testing
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data[0].to(device), data[1].to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+def test(model, loader, device, criterion):
+    model.eval()
+    running_loss = 0.0
+    correct_predictions = 0.0
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            running_loss += loss.item() * inputs.size(0)
+            _, preds = torch.max(outputs, 1)
+            correct_predictions += (preds == labels).sum().item()
+
+    epoch_loss = running_loss / len(loader.dataset)
+    epoch_acc = correct_predictions / len(loader.dataset)
+    
+    return epoch_loss, epoch_acc
+
+if __name__ == "__main__":
+    epochs = 50
+    
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1}/{epochs}")
+        
+        train_loss, train_acc = train_one_epoch(SpikingViT, train_loader, device, criterion, optimizer)
+        print(f"Training Loss: {train_loss:.4f}, Training Acc: {train_acc:.4f}")
+
+        test_loss, test_acc = test(SpikingViT, test_loader, device, criterion)
+        print(f"Testing Loss: {test_loss:.4f}, Testing Acc: {test_acc:.4f}")
+        
+        scheduler.step()
+
